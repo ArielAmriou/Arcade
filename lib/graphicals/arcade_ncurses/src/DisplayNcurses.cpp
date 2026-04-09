353efd22da8f975809b3a42c9aff0c38b65ba647
leroy-antoine <antoine.leroy@epitech.eu>
2026-04-09 18:22:34 +0200

    fix: deleted useless offset in print ncurses

--- lib/graphicals/arcade_ncurses/src/DisplayNcurses.cpp
+++ lib/graphicals/arcade_ncurses/src/DisplayNcurses.cpp
@@ -167,10 +167,8 @@ namespace arc {
             wattron(_window, COLOR_PAIR(color));
             if (entity->getIdx() >= 0 && entity->getIdx() < _assets.size()) {
                 mvwprintw(_window,
-                    static_cast<int>(pos.second * (getmaxy(_window)
-                        - PRINT_OFFSET)),
-                    static_cast<int>(pos.first * (getmaxx(_window)
-                        - PRINT_OFFSET)), "%s",
+                    static_cast<int>(pos.second * getmaxy(_window)),
+                    static_cast<int>(pos.first * getmaxx(_window)), "%s",
                     _assets[entity->getIdx()].data());
             }
             if (entity->getStr())
